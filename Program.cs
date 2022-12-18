53d50b241d71f9874ff21a3c8c06c2ac19f19a6b
mohamed omar <111253955+mohamed2020ie@users.noreply.github.com>
2022-12-18 20:53:25 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace perfect_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the start");
+            int start = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the end");
+            int end = int.Parse(Console.ReadLine());
+            Console.WriteLine(" the perfect numbers are : ");
+
+            for (int i = start; i <= end; i++)
+            {
+                int sum = 0;
+                for (int j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                        sum = sum + j;
+                }
+                if (i == 0) continue;
+                if (sum == i)
+                    Console.WriteLine(i);
+            }
+
+
+        }
+    }
+}
